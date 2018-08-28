f317b9e169d9deb122e8767d9ba179ab0899d3b4
Mikhail K <mk75ru@gmail.com>
2018-08-28 23:17:24 +0700

    добавил файлов в local

---
+++ .emacs_nilsdeppe_and_local.d/local/packages.el
@@ -0,0 +1,24 @@
+(require 'package)
+
+;;; Источники для установки пакетов. Дело в том, что MELPA Stable
+;;; содержит не очень свежие версии, поэтому иногда лучше ставить
+;;; пакеты из основного репозитория.
+(setq package-archives '(("melpa-stable" . "https://stable.melpa.org/packages/")
+                         ("melpa" . "http://melpa.milkbox.net/packages/")
+                         ("gnu" . "http://elpa.gnu.org/packages/")))
+(setq package-enable-at-startup nil)
+(package-initialize nil)
+
+;;; Если пакет use-package не установлен, его нужно скачать и
+;;; установить
+(unless (package-installed-p 'use-package)
+  (message "EMACS install use-package.el")
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+;;; Установили, загрузили, указали, что недостающие пакеты нужно
+;;; автоматически загружать и устанавливать.
+(require 'use-package)
+(setq use-package-always-ensure t)
+
+
