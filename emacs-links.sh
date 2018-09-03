85966c47911d9dc21c8b0c45c89fd453800897a1
Mikhail Krolikov <krolikov@incom.tomsk.ru>
2018-09-03 16:57:35 +0700

    изсенил делание ссылок

--- emacs-links.sh
+++ emacs-links.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
+rm ../.emacs
 rm ../.emacs.el
 ln -s  -T ~/home-settings-mk/.emacs_mk.el  ../.emacs.el
 
-rm ../.emacs.d/local
+rm ../.emacs.d
+ln -s -T ~/.emacs_mk.d  ../.emacs.d
 ln -s -T ~/home-settings-mk/.emacs_nilsdeppe_and_local.d/local  ../.emacs.d/local
 
 rm ../.ycm_extra_conf.py
