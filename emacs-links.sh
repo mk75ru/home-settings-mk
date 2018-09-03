f60d211a8e40f316e14f5d244611d9120059347c
Mikhail Krolikov <krolikov@incom.tomsk.ru>
2018-09-03 09:13:28 +0700

    добавил удаление симлинков перед их созданием

--- emacs-links.sh
+++ emacs-links.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
+
+rm ../.emacs.el
 ln -s  -T ~/home-settings-mk/.emacs_mk.el  ../.emacs.el
 
+rm ../.emacs.d/local
 ln -s -T ~/home-settings-mk/.emacs_nilsdeppe_and_local.d/local  ../.emacs.d/local
 
-
+rm ../.ycm_extra_conf.py
 ln -s  -T ~/home-settings-mk/.ycm_extra_conf_nilsdeppe.py  ../.ycm_extra_conf.py
 
 
