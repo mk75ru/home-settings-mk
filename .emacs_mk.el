58b3fcb0c12fdd43521a533c4a53e6d4de5ebcfa
Mikhail Krolikov <krolikov@incom.tomsk.ru>
2019-08-05 11:06:27 +0700

    сменил каталог для NotesMK

--- .emacs_mk.el
+++ .emacs_mk.el
@@ -203,7 +203,7 @@ easily repeat a find command."
 
 ;;Заметки  C-x rjn
 ;;(set-register ?n (cons 'file "/var/lib/syncthing/NotesMK/index.org")) 
-(set-register ?n (cons 'file "/home/miha/cloud/cloud.files/opn.files/NotesMK")) 
+(set-register ?n (cons 'file "/home/miha/cloud/cloud.files/opn.files/NotesMK/index.org")) 
 
 
 ;;(find-file "/var/lib/syncthing/NotesMK/index.org") 
